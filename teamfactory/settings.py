8b1f020cd0ad7058ca7c411d2e67211b72b61465
anton <kpi.study1@gmail.com>
2018-02-27 21:16:47 +0200

    db and media

--- teamfactory/settings.py
+++ teamfactory/settings.py
@@ -132,8 +132,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
 
 
 EMAIL_USE_TLS = True
