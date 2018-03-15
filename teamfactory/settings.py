6686f21b99033d797a034199d1bbbad90619b79e
anton <kpi.study1@gmail.com>
2018-03-15 20:42:52 +0200

    req.txt

--- teamfactory/settings.py
+++ teamfactory/settings.py
@@ -93,8 +93,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'teamfactory',
-        'USER': 'teamfactory',
-        'PASSWORD': 'teamfactory',
+        'USER': 'admin',
+        'PASSWORD': 'antonmazun',
         'HOST': '',
         'PORT': '',
     }
